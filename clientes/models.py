16b902995e34dac4557ab60a2547afb1e0bd9ee6
delucadev <delucadevbr@gmail.com>
2021-09-30 21:53:24 -0300

    Criação do App Clientes

--- clientes/models.py
+++ clientes/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+
+class Cliente(models.Model):
+    nome_completo = models.CharField(max_length=100)
+    cpf = models.CharField(max_length=20)
+    telefone = models.CharField(max_length=20)
+    email = models.CharField(max_length=100)
